8eb9f996bb01e5efecf97c0028b0524a184e0b9d
Patryk <patryk.r.nowak@gmail.com>
2023-07-02 15:18:57 +0100

    added dynamoDB

--- mainProject/aws-microservices/lib/aws-microservices-stack.ts
+++ mainProject/aws-microservices/lib/aws-microservices-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+
+// OUR MAIN CLASS
 export class AwsMicroservicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMicroservicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // DynamoDB
+    const productTable = new Table(this, 'product', {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING
+      },
+      tableName: "product",
+      // We need to set-up it while studying :>
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      // Pay as you go
+      billingMode: BillingMode.PAY_PER_REQUEST
+    })
+
+
   }
 }
