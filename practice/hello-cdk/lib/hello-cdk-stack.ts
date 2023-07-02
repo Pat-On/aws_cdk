4cfb378c1aef2a7d8f3f54f9773e1af3982d8aae
Patryk <patryk.r.nowak@gmail.com>
2023-07-02 07:10:28 +0100

    adding sqs

--- practice/hello-cdk/lib/hello-cdk-stack.ts
+++ practice/hello-cdk/lib/hello-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +9,16 @@ export class HelloCdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    // Level 2 construct developed by CDK team with roles etc
+    // constructor(scope: Construct, id: string, props?: QueueProps);
+    const queue = new Queue(this, "HelloCdkQueue", {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    })
+
     // example resource
     // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
   }
+
 }
