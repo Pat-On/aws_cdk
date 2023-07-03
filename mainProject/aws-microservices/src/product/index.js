653f41fa5830ee00d3e4ae451b3c7fff1b609978
Patryk <patryk.r.nowak@gmail.com>
2023-07-03 19:55:34 +0100

    added switch method

--- mainProject/aws-microservices/src/product/index.js
+++ mainProject/aws-microservices/src/product/index.js
@@ -1,18 +1,20 @@
-exports.handler = async function(event) {
-    console.log('request:', JSON.stringify(event, undefined, 2));
+exports.handler = async function (event) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
 
-    // TODO - Switch case event.httpmethod to perform CRUD
+  // TODO - Switch case event.httpmethod to perform CRUD
 
-    if(event.httpMethod == "GET") {
-        
-    }
+  switch (event.httpMethod) {
+    case "GET":
+      if (event.pathParameters != null) {
+        body = await getProduct(event.pathParameters.id);
+      } else {
+        body = await getAllProducts();
+      }
+  }
 
-
-
-
-    return {
-        statusCode: 200,
-        headers: { "Content-type" : "text/plain "},
-        body: `Hello from Product! You have hit ${event.path} \n`
-    };  
+  return {
+    statusCode: 200,
+    headers: { "Content-type": "text/plain " },
+    body: `Hello from Product! You have hit ${event.path} \n`,
+  };
 };
