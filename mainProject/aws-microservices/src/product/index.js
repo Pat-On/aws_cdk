4038ce648e3839eb0b42847fecb5d988de914e74
Patryk <patryk.r.nowak@gmail.com>
2023-07-04 07:02:32 +0100

    get products by category

--- mainProject/aws-microservices/src/product/index.js
+++ mainProject/aws-microservices/src/product/index.js
@@ -13,11 +13,15 @@ exports.handler = async function (event) {
 
     switch (event.httpMethod) {
         case "GET":
-            if (event.pathParameters != null) {
-                body = await getProduct(event.pathParameters.id);
-            } else {
-                body = await getAllProducts();
-            }
+            if(event.queryStringParameters != null) {
+                body = await getProductsByCategory(event); // GET product/1234?category=Phone
+              }
+              else if (event.pathParameters != null) {
+                body = await getProduct(event.pathParameters.id); // GET product/{id}
+              } else {
+                body = await getAllProducts(); // GET product
+              }
+              break;
         case "POST":
             // demo - no sanitization
             body = await createProduct(event);
@@ -28,6 +32,8 @@ exports.handler = async function (event) {
         case "UPDATE":
             body = await updateProduct(event);
             break;
+        default:
+            throw new Error(`Unsupported route: "${event.httpMethod}"`);
     }
 
     return {
@@ -131,30 +137,83 @@ const deleteProduct = async (productId) => {
 const updateProduct = async (event) => {
     console.log(`updateProduct function. event : "${event}"`);
     try {
-      const requestBody = JSON.parse(event.body);
-      const objKeys = Object.keys(requestBody);
-      console.log(`updateProduct function. requestBody : "${requestBody}", objKeys: "${objKeys}"`);    
-  
-      const params = {
-        TableName: process.env.DYNAMODB_TABLE_NAME,
-        Key: marshall({ id: event.pathParameters.id }),
-        UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
-        ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
-            ...acc,
-            [`#key${index}`]: key,
-        }), {}),
-        ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
-            ...acc,
-            [`:value${index}`]: requestBody[key],
-        }), {})),
-      };
-  
-      const updateResult = await ddbClient.send(new UpdateItemCommand(params));
-  
-      console.log(updateResult);
-      return updateResult;
-    } catch(e) {
-      console.error(e);
-      throw e;
+        const getProductsByCategory = async (event) => {
+            console.log("getProductsByCategory");
+            try {
+                // GET product/1234?category=Phone
+                const productId = event.pathParameters.id;
+                const category = event.queryStringParameters.category;
+
+                const params = {
+                    KeyConditionExpression: "id = :productId",
+                    FilterExpression: "contains (category, :category)",
+                    ExpressionAttributeValues: {
+                        ":productId": { S: productId },
+                        ":category": { S: category }
+                    },
+                    TableName: process.env.DYNAMODB_TABLE_NAME
+                };
+
+                const { Items } = await ddbClient.send(new QueryCommand(params));
+
+                console.log(Items);
+                return Items.map((item) => unmarshall(item));
+            } catch (e) {
+                console.error(e);
+                throw e;
+            }
+        }
+        const requestBody = JSON.parse(event.body);
+        const objKeys = Object.keys(requestBody);
+        console.log(`updateProduct function. requestBody : "${requestBody}", objKeys: "${objKeys}"`);
+
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ id: event.pathParameters.id }),
+            UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+            ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
+                ...acc,
+                [`#key${index}`]: key,
+            }), {}),
+            ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
+                ...acc,
+                [`:value${index}`]: requestBody[key],
+            }), {})),
+        };
+
+        const updateResult = await ddbClient.send(new UpdateItemCommand(params));
+
+        console.log(updateResult);
+        return updateResult;
+    } catch (e) {
+        console.error(e);
+        throw e;
     }
-  }
+}
+
+const getProductsByCategory = async (event) => {
+    console.log("getProductsByCategory");
+    try {
+        // GET product/1234?category=Phone
+        const productId = event.pathParameters.id;
+        const category = event.queryStringParameters.category;
+
+        const params = {
+            KeyConditionExpression: "id = :productId",
+            FilterExpression: "contains (category, :category)",
+            ExpressionAttributeValues: {
+                ":productId": { S: productId },
+                ":category": { S: category }
+            },
+            TableName: process.env.DYNAMODB_TABLE_NAME
+        };
+
+        const { Items } = await ddbClient.send(new QueryCommand(params));
+
+        console.log(Items);
+        return Items.map((item) => unmarshall(item));
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
